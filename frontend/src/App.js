3baf9bb77c7221792b194baaecb0bd63cccbaa06
Shivansh Raghav <shivansh901@gmail.com>
2022-06-02 01:13:36 +0530

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/footer';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -17,8 +18,8 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
+          <Footer />
         </div>
-
       </Router>
       <ToastContainer />
     </>
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <small>A Goal Setter app made with MERN Stack</small> <br />  
+      Shivansh Raghav - <a href="https://www.linkedin.com/in/shiv901/">Linked In</a> | <a href="https://www.github.com/shiv901">Github</a>
+    </div>
+  );
+}
+
+export default Footer;
